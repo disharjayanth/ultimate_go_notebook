164b8bd7852a3df0eb5d2b8d87e317f7e5363fa4
disharjayantha <dishuj15@gmail.com>
2021-10-11 17:01:37 +0530

    drop pattern

---
+++ chap06/11_drop/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func drop() {
+	const cap = 100
+	ch := make(chan string, cap)
+
+	go func() {
+		for str := range ch {
+			fmt.Printf("child: received signal: %s\n", str)
+		}
+	}()
+
+	for i := 0; i < 2000; i++ {
+		select {
+		case ch <- "data":
+			fmt.Printf("parent: sent signal: %d\n", i)
+		default:
+			fmt.Printf("parent: dropped data: %d\n", i)
+		}
+	}
+
+	close(ch)
+
+	fmt.Println("parent: shutdown signal.....")
+}
+
+func main() {
+	drop()
+}
