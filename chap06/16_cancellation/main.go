f8ff7afe8cffb8a1fa8eb7c10165a90be66e4f73
disharjayantha <dishuj15@gmail.com>
2021-10-13 10:52:37 +0530

    cancelling pattern

---
+++ chap06/16_cancellation/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func channelCancellation(stop <-chan struct{}) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		select {
+		case <-stop:
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	func(ctx context.Context) error {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://www.ardanlabs.com/blog/index.xml", nil)
+		if err != nil {
+			return err
+		}
+
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+
+		sb, _ := ioutil.ReadAll(res.Body)
+		fmt.Println("Response:", string(sb))
+		return nil
+	}(ctx)
+}
+
+func main() {
+	stop := make(chan struct{})
+	channelCancellation(stop)
+}
